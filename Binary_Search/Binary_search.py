65cc35906d1168ef7fb2f70a44b66e13fc241666
Warenga <warengam@gmail.com>
2016-11-15 12:16:49 +0300

    binary search large list tests pass

--- Binary_Search/Binary_search.py
+++ Binary_Search/Binary_search.py
@@ -1,37 +1,37 @@
 class BinarySearch(list):
 	def __init__(self, list_len, step):
-		self.liste = range(0+step, list_len*step+1, step)
-		list.__init__(self, self.liste)
- 		self.length = len(self.liste)
-
- 	def __repr__(self):
-		return repr(range(0, self.list_len, self.step))
-
+		self.listnum = range(0+step, list_len*step+1, step)
+		list.__init__(self, self.listnum)
+ 		self.length = len(self.listnum)
+ 	
 	def search(self, item):
 		first = 0
-		last = len(self.liste)
+		last = len(self.listnum) - 1
 		found = False
 		count = 0
 
-		if item not in self.liste:
+		if item not in self.listnum:
 			return {'count': count, 'index' : -1}
 		else:
 			while first <= last and not found:
 				midpoint = (first + last) // 2
-				if self.liste[midpoint] == item:
+				
+				if self.listnum[first] == item:
+					return {'count': count, 'index' : first}
+				elif self.listnum[last] == item:
+					return {'count': count, 'index' : last}
+				elif self.listnum[midpoint] == item:
 					count += 1
 					found = True
 				else:
-					if item < self.liste[midpoint]:
+					if item < self.listnum[midpoint]:
 						count += 1
 						last = midpoint - 1
 					else:
 						count += 1
 						first = midpoint + 1
-			return {'count': count, 'index' : self.liste.index(item)}
+			return {'count': count, 'index' : self.listnum.index(item)}
 
-ten_to_thousand = BinarySearch(100, 10)
-search1 = ten_to_thousand.search(880)
-print search1
-
-
+ten_to_thousand = BinarySearch(20, 2)
+search2 = ten_to_thousand.listnum
+print search2
